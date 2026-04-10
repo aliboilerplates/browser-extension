cfe54dd3cf83a08aa2c6f7c1a2ecd22276e92747
muhammadali <muhammadalihassanofficial@gmail.com>
2026-04-10 11:50:12 +0500

    chore(offscreen): remove unused scaffolding

--- src/entrypoints/offscreen/index.ts
+++ src/entrypoints/offscreen/index.ts
@@ -1,6 +1,1 @@
-import { sendMessage } from "@/core/messaging";
-
-// Offscreen remains optional scaffolding in v1.
-
-
-const res = await sendMessage("core/updateSettings", { theme: "dark" });
+// eslint-disable-next-line unicorn/no-empty-file
